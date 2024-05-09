fbbada0d9dbfddbae543b947f84909fa585402e6
sergiolix <sergiolix8@gmail.com>
2024-05-08 23:42:34 -0500

    un Poco de Css

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CentralComponent } from './central/central.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , NavbarComponent],
+  imports: [RouterOutlet , NavbarComponent , CentralComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
